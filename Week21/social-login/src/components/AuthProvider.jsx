576e15277140d946e4e14b17556c0208dc6793d4
YeryungCho <yeryung1128@gmail.com>
2024-11-30 01:36:47 +0900

    배포 주소 수정2

--- Week21/social-login/src/components/AuthProvider.jsx
+++ Week21/social-login/src/components/AuthProvider.jsx
@@ -3,8 +3,7 @@ import React, { createContext, useState, useContext } from "react";
 const AuthContext = createContext();
 const GOOGLE_CLIENT_ID =
   "266787898619-gra6mtvc723pr8qsh65dkc8eb77leh7d.apps.googleusercontent.com";
-const REDIRECT_URI =
-  "https://social-login-glndk2xfp-yeryungchos-projects.vercel.app"; // 배포 시 수정 필요
+const REDIRECT_URI = "https://social-login-yeryungchos-projects.vercel.app/"; // 배포 시 수정 필요
 
 export function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
